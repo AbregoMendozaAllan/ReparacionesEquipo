d7d3b0fc3e40635d61eadeb21758502a768c3f7d
AbregoMendozaAllan <153949305+AbregoMendozaAllan@users.noreply.github.com>
2025-04-02 11:34:32 -0600

    correccion de una funcion en equiposController.js (crearNuevoEquipo) y limpieza de imports

--- src/controllers/equiposController.js
+++ src/controllers/equiposController.js
@@ -1,4 +1,4 @@
-import {createEquipo, deleteEquipo, getAllEquipos, getEquipoById, updateEquipo,} from "../dao/equiposDao.js";
+import {createEquipo, deleteEquipo, getAllEquipos, getEquipoById,} from "../dao/equiposDao.js";
 
 // Mostrar lista de equipos
   export const mostrarEquipos = async (req, res) => {
@@ -44,35 +44,7 @@ import {createEquipo, deleteEquipo, getAllEquipos, getEquipoById, updateEquipo,}
       res.status(500).send("Error al crear equipo");
     }
   };
-  
-  // Mostrar formulario para editar equipo
-  export const formularioEditarEquipo = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const equipo = await getEquipoById(id);
-      if (!equipo) {
-        return res.status(404).send("Equipo no encontrado");
-      }
-      res.render("equipos/editar", { equipo });
-    } catch (error) {
-      console.error("Equipo no se pudo ingresar", error);
-      res.status(500).send("Error al obtener equipo para editar");
-    }
-  };
-  
-  // Actualizar equipo
-  export const actualizarEquipo = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { nombre, descripcion, fecha_creacion } = req.body;
-      await updateEquipo(id, nombre, descripcion, fecha_creacion);
-      res.redirect("/equipos");
-    } catch (error) {
-      console.error("Error al actualizar equipo:", error);
-      res.status(500).send("Error al actualizar equipo");
-    }
-  };
-  
+
   // Eliminar equipo
   export const eliminarEquipo = async (req, res) => {
     try {
--- src/routes/equipos.routes.js
+++ src/routes/equipos.routes.js
@@ -6,8 +6,6 @@ const router = express.Router();
 router.get("/", EquiposController.mostrarEquipos); // Listado de equipos
 router.get("/create", EquiposController.formularioCrearEquipo); // Formulario para crear equipo
 router.post("/create", EquiposController.crearNuevoEquipo); // Crear nuevo equipo
-router.get("/edit/:id", EquiposController.formularioEditarEquipo); // Formulario para editar equipo
-router.post("/edit/:id", EquiposController.actualizarEquipo); // Actualizar equipo
 router.post("/delete/:id", EquiposController.eliminarEquipo); // Eliminar equipo
 
 export default router;
